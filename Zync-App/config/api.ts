58138ec3f88c9c7849fef3a778636bc9f8281593
YashDoshi01 <90956721+YashDoshi01@users.noreply.github.com>
2025-05-18 13:24:51 +0530

    added the production backend url
    using the production url for the api calling

--- Zync-App/config/api.ts
+++ Zync-App/config/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.API_URL || "http://localhost:5000/api", // fallback for dev
-});
+  baseURL: process.env.API_URL || "https://zync-production.up.railway.app/api", // fallback for dev
+ });
 
 export default api;
